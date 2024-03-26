1d936f97705f1bd37578ad36e6a30e367a531888
jserrataylor <jose.serra@upr.edu>
2024-03-26 17:39:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-from flask import Flask, jsonify, request
-from sklearn.externals import joblib
-import sklearn
-
-
-#curl -d "{\"Medidas\":[[1,2,3,4]]}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/predecir
-
-app= Flask(__name__)
-
-@app.route("/")
-def home():
-    return 'La pagina esta funcionando bien'
-
-@app.route("/predecir", methods=["POST"])
-def predecir():
-    json=request.get_json(force=True)
-    medidas=json['Medidas']
-    clf=joblib.load('Modelo_Entrenado.pkl')
-    prediccion=clf.predict(medidas)
-    return 'Las medidas que diste corresponden a la clase {0}\n\n'.format(prediccion)
-
-if __name__ == '__main__':
-    app.run()
-
-'''
 from flask import Flask, request, render_template_string, jsonify, url_for
 import sklearn
 from sklearn.externals import joblib
@@ -95,6 +70,4 @@ def index():
 
 if __name__ == '__main__':
     app.run()
-
-'''
 
